a345b1cdb46618fadda1ec8b3e05342da36f5da6
ovie umukoro <ovie.u.umukoro@gmail.com>
2020-06-23 23:05:46 -0400

    Test the signup page view

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_path
     assert_response :success
+    assert_template 'users/new'
   end
 
 end
