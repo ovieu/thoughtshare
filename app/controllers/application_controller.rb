0973062703107dbb6075b7aa944cb32a834f8eda
ovie umukoro <ovie.u.umukoro@gmail.com>
2020-06-23 23:01:54 -0400

    Save the current user to the client session cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   def hello
     render html: "hello, world"
+  end
+
+  def current_user
+    if @current_user.nil?
+      @current_user = User.find_by(id: session[:user_id])
+    end
+    if session[:user_id]
+      User.find_by(id: session[:user_id])
+    end
   end
 end
 
