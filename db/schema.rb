614ac7c50a1793f541d3a3eb802a18a2f4564714
ovie umukoro <ovie.u.umukoro@gmail.com>
2020-07-01 06:53:54 -0400

    Add activation digest to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_000610) do
+ActiveRecord::Schema.define(version: 2020_08_07_210028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_000610) do
     t.string "remember_digest"
     t.boolean "admin"
     t.string "activation_digest"
-    t.boolean "activated"
+    t.boolean "activated", default: false
     t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #   Create a main sample user
-User.create!(name:  "Example User",
+User.create!(name: "Example User",
              email: "example@railstutorial.org",
-             password:              "foobar",
+             password: "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
 
 # Generate a bunch of additional users.
 99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  name = Faker::Name.name
+  email = "example-#{n + 1}@railstutorial.org"
   password = "password"
-  User.create!(name:  name,
+  User.create!(name: name,
                email: email,
-               password:              password,
-               password_confirmation: password)
+               password: password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
