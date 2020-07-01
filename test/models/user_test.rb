ad6b431d8aec263c9d58423688fdbfb43b8e307b
ovie umukoro <ovie.u.umukoro@gmail.com>
2020-07-01 07:09:52 -0400

    Authenticate new user based on activation token

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -68,6 +68,5 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? shouhld return false for a user with nil digest" do
-    assert_not @user.authenticated?("")
-  end
+    assert_not @user.authenticated?(:remember, '')  end
 end
