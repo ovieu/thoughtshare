529851d737d0eb6e0c35e41c30b00d91cc476d5f
ovie umukoro <ovie.u.umukoro@gmail.com>
2020-07-01 07:14:58 -0400

    Refactor activation test

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,17 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   # Returns true if the given token matches the digest.
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
