e7bb8f14578dc008743586a5b3601df77e5b64bc
ovie umukoro <ovie.u.umukoro@gmail.com>
2020-07-02 10:37:58 -0400

    Add index to user table

--- db/schema.rb
+++ db/schema.rb
@@ -24,15 +24,6 @@ ActiveRecord::Schema.define(version: 2020_08_09_012633) do
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.text "content"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -50,5 +41,4 @@ ActiveRecord::Schema.define(version: 2020_08_09_012633) do
   end
 
   add_foreign_key "microposts", "users"
-  add_foreign_key "posts", "users"
 end
