e5b18a6d707cdb9a53d231e2baa95158322acc05
ovie umukoro <ovie.u.umukoro@gmail.com>
2020-07-02 11:22:00 -0400

    Setup the posts controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,15 @@ class ApplicationController < ActionController::Base
       User.find_by(id: session[:user_id])
     end
   end
+
+  private
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 end
 
